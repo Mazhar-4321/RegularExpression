6ec471be74d97d7ea8e9b7c636e69c917067973d
Mazhar-4321 <syedmazharali742@gmail.com>
2022-12-11 17:46:28 +0530

    ADDED: Code to Validate Last Name Using Regular Expression

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private User user = new User();
+
     public static void main(String[] args) {
         System.out.println("Welcome To Regular Expressions Program");
         Main main = new Main();
@@ -13,18 +14,19 @@ public class Main {
 
     private void validateUserRegistration() {
         validateName("firstName");
+        validateName("lastName");
     }
 
     private void validateName(String firstNameOrLastName) {
-        System.out.println("Please Enter Your First Name");
+        System.out.println("Please Enter Your " + firstNameOrLastName);
         String name = scanner.next();
-        String pattern ="^[A-Z][a-zA-Z0-9[^a-zA-Z0-9]]{2,}";
+        String pattern = "^[A-Z][a-zA-Z0-9[^a-zA-Z0-9]]{2,}";
         while (!name.matches(pattern)) {
-            System.out.println("Invalid "+firstNameOrLastName+", it must start with Uppercase Alphabet followed by atleast two characters");
-            System.out.println("Please Re enter your "+firstNameOrLastName);
-            name=scanner.next();
+            System.out.println("Invalid " + firstNameOrLastName + ", it must start with Uppercase Alphabet followed by atleast two characters");
+            System.out.println("Please Re enter your " + firstNameOrLastName);
+            name = scanner.next();
         }
-        System.out.println("The "+firstNameOrLastName+" entered is registered successfully");
+        System.out.println("The " + firstNameOrLastName + " entered is registered successfully");
         user.setFirstName(name);
     }
 }
